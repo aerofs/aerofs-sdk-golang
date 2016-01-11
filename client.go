94abcd236923842054ed258e647f1954eca9ca1f
Daniel Cardoza <daniel.cardoza@aerofs.com>
2016-01-11 15:34:31 -0800

    [groupMembers] Basic groupMember functionality complete

--- client.go
+++ client.go
@@ -366,3 +366,70 @@ func (c *Client) DeleteGroup(groupID string) error {
 	defer res.Body.Close()
 	return err
 }
+
+// GroupMember Functions
+func (c *Client) ListGroupMembers(groupID string) (*[]GroupMember, error) {
+	link := url.URL{Scheme: "https",
+		Host: c.Host,
+		Path: strings.Join([]string{API, "groups", groupID, "members"}, "/"),
+	}
+
+	res, err := c.get(link.String())
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	groupMembers := []GroupMember{}
+	err = GetEntity(res, &groupMembers)
+	return &groupMembers, err
+}
+
+func (c *Client) AddGroupMember(groupID string, groupMember GroupMember) (*GroupMember, error) {
+	link := url.URL{Scheme: "https",
+		Host: c.Host,
+		Path: strings.Join([]string{API, "groups", groupID, "members"}, "/"),
+	}
+
+	data, err := json.Marshal(groupMember)
+	if err != nil {
+		return nil, errors.New("Unable to marshal provided group member")
+	}
+
+	res, err := c.post(link.String(), bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	newMember := GroupMember{}
+	err = GetEntity(res, &newMember)
+	return &newMember, err
+}
+
+func (c *Client) GetGroupMember(groupID, email string) (*GroupMember, error) {
+	link := url.URL{Scheme: "https",
+		Host: c.Host,
+		Path: strings.Join([]string{API, "groups", groupID, "members", email}, "/"),
+	}
+
+	res, err := c.get(link.String())
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	groupMember := GroupMember{}
+	err = GetEntity(res, &groupMember)
+	return &groupMember, err
+}
+
+func (c *Client) RemoveMember(groupID, email string) error {
+	link := url.URL{Scheme: "https",
+		Host: c.Host,
+		Path: strings.Join([]string{API, "groups", groupID, "members", email}, "/"),
+	}
+
+	res, err := c.del(link.String())
+	defer res.Body.Close()
+	return err
+}
