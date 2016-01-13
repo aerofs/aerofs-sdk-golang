89642eb7b7afa406955ef4c1060d84245cb7704e
Daniel Cardoza <daniel.cardoza@aerofs.com>
2016-01-12 23:03:58 -0800

    [refactor][invitations] Refactored invitations

--- invitation.go
+++ invitation.go
@@ -1,56 +1,56 @@
 package aerofs
 
 import (
+	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
 // Return a list of Invitations to shared folders for a given user
-func (c *Client) ListSFInvitations(email string) (*[]Invitation, error) {
+func (c *Client) ListSFInvitations(email string) (*[]byte, *http.Header, error) {
 	route := strings.Join([]string{"users", email, "invitations"}, "/")
 	link := c.getURL(route, "")
 
 	res, err := c.get(link)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	invitations := []Invitation{}
-	err = GetEntity(res, &invitations)
-	return &invitations, err
+	body, header := unpackageResponse(res)
+	return body, header, err
 }
 
 // View invitation metadata for a pending invitation
-func (c *Client) ViewPendingSFInvitation(email, sid string) (*Invitation, error) {
+func (c *Client) ViewPendingSFInvitation(email, sid string) (*[]byte, *http.Header, error) {
 	route := strings.Join([]string{"users", email, "invitations", sid}, "/")
 	link := c.getURL(route, "")
 
 	res, err := c.get(link)
 	defer res.Body.Close()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	invitation := Invitation{}
-	err = GetEntity(res, &invitation)
-	return &invitation, err
+	body, header := unpackageResponse(res)
+	return body, header, err
 }
 
 // Accept a pending Shared Folder invitation
-func (c *Client) AcceptSFInvitation(email, sid string) (*SharedFolder, error) {
+func (c *Client) AcceptSFInvitation(email, sid string, external int) (*[]byte, *http.Header, error) {
 	route := strings.Join([]string{"users", email, "invitations", sid}, "/")
-	query := url.Values{"external": []string{"0"}}
+	query := url.Values{}
+	query.Set("external", strconv.Itoa(external))
 	link := c.getURL(route, query.Encode())
 
-	res, err := c.get(link)
+	res, err := c.post(link, nil)
 	defer res.Body.Close()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	sf := SharedFolder{}
-	err = GetEntity(res, &sf)
-	return &sf, err
+	body, header := unpackageResponse(res)
+	return body, header, err
 }
 
 // Ignore an existing invitation to a shared folder
