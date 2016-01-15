c2b324bf0b151058f2b7e1110e42d19303418bf6
Daniel Cardoza <daniel.cardoza@aerofs.com>
2016-01-15 12:08:30 -0800

    [refactor] Refactored Authorization to make Authentication flow easier
    - Added constructor to build an AuthClient from the AeroFS generated
      appconfig.json
    - Modified existing Authclient to remove redundant config struct

--- aerofsapi/auth.go
+++ aerofsapi/auth.go
@@ -5,49 +5,94 @@ package aerofsapi
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
-// Application Specific Information
-type AuthConfig struct {
-	Id       string
-	Secret   string
-	Redirect string
-	Scopes   []string
-}
-
-type Access struct {
-	Token      string `json:"access_token"`
-	TokenType  string `json:"token_type"`
-	ExpireTime int    `json:"expires_in"`
-	Scopes     string `json:"scope"`
-}
-
-// Authorization Interface
+// Interface between an application and the AeroFS server
+// Can be constructed from a generated AeroFS Config Json file
+// Note that the Redirect URL must be the same as that stored when registering
+// the third party application
 type AuthClient struct {
-	// Parameters required for network requests
-	Config AuthConfig
-
 	// The URL of an AeroFS Appliance instance
-	AppUrl string
+	AeroUrl string `json:"hostname"`
+	Id      string `json:"client_id"`
+	Secret  string `json:"client_secret"`
+
+	// The URL and route  the user will be redirected to after accepting scope permissions on
+	// the AeroFS Appliance
+	Redirect string
+
+	// Permission scopes
+	Scopes []string
+
+	// A unique identifier created by the 3rd-Party App and eventually passed back
+	// to it after the user has confirmed scopes on the AeroFS Appliance end
+	State string
 }
 
-// Return the destination URL for user 3rd-party app authorization
-func (auth *AuthClient) GetAuthCode() string {
-	scopes := strings.Join(auth.Config.Scopes, ",")
+// The response when receiving a token given an authorization code
+type Access struct {
+	// OAuth 2.0 Token
+	Token string `json:"access_token"`
+
+	// Default is "Bearer"
+	TokenType string `json:"token_type"`
+
+	// Number of seconds until token expiration
+	ExpireTime int `json:"expires_in"`
+
+	// User scopes associated with this token
+	Scopes string `json:"scope"`
+}
+
+// Create a new AuthClient from an AeroFS appconfig.Json file
+// The appconfig.json only contains the AeroURL and client_{id,secret}
+func NewAuthClient(fileName, redirectUri, state string, scopes []string) (*AuthClient, error) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, errors.New("Unable to read the defined file")
+	}
+
+	// Unmarshalling will not reset these fields
+	authClient := AuthClient{
+		Redirect: redirectUri,
+		State:    state,
+		Scopes:   scopes,
+	}
+	err = json.Unmarshal(data, &authClient)
+	if err != nil {
+		return nil, errors.New("Unable to unmarshal the requested appconfig.json")
+	}
+
+	return &authClient, nil
+}
+
+// Return a URL to the AeroFS Appliance so the user can authorize third-party
+// access
+func (auth *AuthClient) GetAuthorizationUrl() string {
+	scopes := strings.Join(auth.Scopes, ",")
 	v := url.Values{}
 	v.Set("response_type", "code")
-	v.Set("client_id", auth.Config.Id)
-	v.Set("redirect_uri", auth.Config.Redirect)
+	v.Set("client_id", auth.Id)
+	v.Set("redirect_uri", auth.Redirect)
 	v.Set("scope", scopes)
-	query := v.Encode()
-	route := "authorize"
+	if auth.State != "" {
+		v.Set("state", auth.State)
+	}
 
-	url := fmt.Sprintf("https://%v/%v?%v", auth.AppUrl, route, query)
-	return url
+	route := "authorize"
+	link := url.URL{Scheme: "https",
+		Host:     auth.AeroUrl,
+		Path:     route,
+		RawQuery: v.Encode(),
+	}
+
+	return link.String()
 }
 
 // Retrieve User OAuth token, scopes given an Authorization code
@@ -57,12 +102,12 @@ func (auth *AuthClient) GetAccessToken(code string) (string, []string, error) {
 	v := url.Values{}
 	v.Set("grant_type", "authorization_code")
 	v.Set("code", code)
-	v.Set("client_id", auth.Config.Id)
-	v.Set("client_secret", auth.Config.Secret)
-	v.Set("redirect_uri", auth.Config.Redirect)
+	v.Set("client_id", auth.Id)
+	v.Set("client_secret", auth.Secret)
+	v.Set("redirect_uri", auth.Redirect)
 
 	link := url.URL{Scheme: "https",
-		Host: auth.AppUrl,
+		Host: auth.AeroUrl,
 		Path: strings.Join([]string{"auth", "token"}, "/"),
 	}
 	body := bytes.NewBuffer([]byte(v.Encode()))
